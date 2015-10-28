61cb7aedab924a2f9bd74da9339645cbe55ceb2c
Benjamin LeRoy <benjaminleroy@berkeley.edu>
2015-10-27 21:33:34 -0700

    Added an additional test function from class work.

--- code/test_files/test_hypothesis.py
+++ code/test_files/test_hypothesis.py
@@ -14,14 +14,14 @@ from numpy.testing import assert_almost_equal, assert_array_equal
 
 # Path to the subject 009 fMRI data used in class.  
 # Path to functions. 
-pathtoclassdata = "../../../data/ds114/"
-
+pathtoclassdata = "../../data/ds114/"
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "functions"))
 
 # Load our t_stat functions. 
 from hypothesis import t_stat
 
-def test_hypothesis():
+
+def test_hypothesis1():
     img = nib.load(pathtoclassdata + "ds114_sub009_t2r1.nii")
     data = img.get_data()[..., 4:]
     # Read in the convolutions. 
@@ -34,10 +34,27 @@ def test_hypothesis():
     assert_almost_equal(beta,beta2)
     assert t.all() == t2.all()
     assert beta.shape[1] == np.prod(data.shape[0:-1]) 
-    
-    
 
-    
+
+def test_hypothesis2():
+    # example from http://www.jarrodmillman.com/rcsds/lectures/glm_intro.html
+    # it should be pointed out that hypothesis just looks at simple linear regression
+
+    psychopathy = [11.416,   4.514,  12.204,  14.835,
+    8.416,   6.563,  17.343, 13.02,
+    15.19 ,  11.902,  22.721,  22.324]
+    clammy = [0.389,  0.2  ,  0.241,  0.463,
+    4.585,  1.097,  1.642,  4.972,
+    7.957,  5.585,  5.527,  6.964]  
+
+    Y = np.asarray(psychopathy)
+    B, t, df, p = t_stat(Y, clammy, [0, 1])
+
+
+    assert np.round(t,6)==np.array([[ 1.914389]])
+    assert np.round(p,6)==np.array([[ 0.042295]])
+
+
     
     
     
