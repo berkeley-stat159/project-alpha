c766de17a4e836e5818511efca9781755ce31442
Jane Liang <jwliang@berkeley.edu>
2015-10-29 10:36:55 -0700

    Added test.

--- data/get_hashes.py
+++ data/get_hashes.py
@@ -2,28 +2,28 @@ import os
 import json
 from data import generate_file_md5
 
-def get_hashes(data_dir, output_file):
+def get_all_hashes(data_dir):
     """
     Gets all files in all subdirectories of the supplied directory. 
     Creates a dictionary where the paths to the files are the keys 
     and the values are their corresponding hashes. 
     
-    Returns nothing, but dumps the dictionary to a JSON file.
+    Returns file_hash, a dictionary of hashes. 
     
     Parameters
     ----------
     data_dir: path to a directory
         The directory to search for files.
-    output_file: JSON file name
-        The json file to dump the resulting dictionary.
     """
     file_hashes = {}
     for root, dirs, files in os.walk(data_dir):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             file_hashes[file_path] = generate_file_md5(file_path)
-    with open(output_file, 'w') as out:
-        json.dump(file_hashes, out)
+    return file_hashes 
+
 
 if __name__ == "__main__":
-    get_hashes('ds009', 'ds009_hashes.json')
+    file_hashes = get_all_hashes('ds009')
+    with open('ds009_hashes.json', 'w') as out:
+        json.dump(file_hashes, out)
--- data/tests/test_data.py
+++ data/tests/test_data.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 import tempfile
+import os
 
 from .. import data
+from .. import get_hashes
 
 
 def test_check_hashes():
@@ -14,3 +16,7 @@ def test_check_hashes():
         assert data.check_hashes(d)
         d = {fname: "4b82f8bf4df2bfb0e66ccaa7306fd024"}
         assert not data.check_hashes(d)
+
+def test_get_hashes(): 
+    file_hashes = get_hashes.get_all_hashes('.')
+    assert(file_hashes['./__init__.py'] == 'd41d8cd98f00b204e9800998ecf8427e')
