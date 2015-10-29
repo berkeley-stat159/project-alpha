2e345e81da406f9015771e065562f737b04ed793
Jane Liang <jwliang@berkeley.edu>
2015-10-28 23:35:49 -0700

    Deleted those two lines.

--- data/tests/test_data.py
+++ data/tests/test_data.py
@@ -6,8 +6,6 @@ from .. import data
 
 
 def test_check_hashes():
-    tf = tempfile.NamedTemporaryFile(delete=False)
-    fname = tf.name
     with tempfile.NamedTemporaryFile() as temp:
         temp.write(b'Some data')
         temp.flush()
