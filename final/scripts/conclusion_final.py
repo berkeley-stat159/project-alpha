561485b1b9ff40a9865c310afb8262231d48ff85
hiroto-udagawa <hiroto.udagawa@berkeley.edu>
2015-11-30 10:52:23 -0800

    saved clustering images

--- final/scripts/conclusion_final.py
+++ final/scripts/conclusion_final.py
@@ -45,100 +45,101 @@ from benjamini_hochberg import bh_procedure
 
 #Mean across all subject
 
-# t_mean = np.zeros((64, 64, 34,24))
-#
-# #loop through each person's T-statistic
-# count=0
-# for i in sub_list:
-#
-#     t_stat = np.load(t_data+i+"_tstat.npy")
-#     mask = nib.load(path_to_data+i+'/anatomy/inplane001_brain_mask.nii.gz')
-#     mask_data = mask.get_data()
-#
-#     t_mean[...,count] = make_mask(t_stat, mask_data, fit=True)
-#     count+=1
-#
-# t_mean = np.mean(t_mean,axis=3)
-# final = present_3d(t_mean)
-# plt.imshow(final,interpolation='nearest', cmap='seismic')
-# plt.title("Mean T-Statistic Value Across 25 Subjects")
-#
-# zero_out=max(abs(np.min(final)),np.max(final))
-# plt.clim(-10,10)
-# plt.colorbar()
-# plt.show()
-#
-# #Cluster
-#
-# data_new = t_mean[...,10:15]
-# X = np.reshape(data_new, (-1, 1))
-#
-# connectivity = grid_to_graph(n_x= data_new.shape[0], n_y = data_new.shape[1], n_z = data_new.shape[2])
-#
-# n_clusters = 8 # number of regions
-# ward = AgglomerativeClustering(n_clusters=n_clusters,
-#         linkage='ward', connectivity=connectivity).fit(X)
-# label = np.reshape(ward.labels_, data_new.shape)
-#
-# label_mean = np.zeros(n_clusters)
-# center = list()
-#
-# #FIND THE AVERAGE T-VALUE PER CLUSTER
-# for j in range(n_clusters):
-#     mask = label==j
-#     index = np.where(mask)
-#     center.append((np.mean(index[0]),np.mean(index[1]),np.mean(index[2])))
-#     label_mean[j] =np.mean(data_new[mask])
-#
-# #PRINT THE PLOTS
-# for i in range(data_new.shape[-1]):
-#     plt.figure()
-#     plt.imshow(data_new[...,i], cmap=plt.cm.gray)
-#     for l in range(n_clusters):
-#         plt.contour(label[...,i] == l, contours=1,
-#             colors=[plt.cm.spectral(l / float(n_clusters)), ],linewidths= 0.4)
-# plt.xticks(())
-# plt.yticks(())
-# plt.show()
+t_mean = np.zeros((64, 64, 34,24))
+
+#loop through each person's T-statistic
+count=0
+for i in sub_list:
+
+    t_stat = np.load(t_data+i+"_tstat.npy")
+    mask = nib.load(path_to_data+i+'/anatomy/inplane001_brain_mask.nii.gz')
+    mask_data = mask.get_data()
+
+    t_mean[...,count] = make_mask(t_stat, mask_data, fit=True)
+    count+=1
+
+t_mean = np.mean(t_mean,axis=3)
+final = present_3d(t_mean)
+plt.imshow(final,interpolation='nearest', cmap='seismic')
+plt.title("Mean T-Statistic Value Across 25 Subjects")
+
+zero_out=max(abs(np.min(final)),np.max(final))
+plt.clim(-10,10)
+plt.colorbar()
+plt.show()
+plt.close()
+
+#Cluster
+
+data_new = t_mean[...,20:23]
+X = np.reshape(data_new, (-1, 1))
+
+connectivity = grid_to_graph(n_x= data_new.shape[0], n_y = data_new.shape[1], n_z = data_new.shape[2])
+
+n_clusters = 8 # number of regions
+ward = AgglomerativeClustering(n_clusters=n_clusters,
+        linkage='ward', connectivity=connectivity).fit(X)
+label = np.reshape(ward.labels_, data_new.shape)
+
+label_mean = np.zeros(n_clusters)
+center = list()
+
+#FIND THE AVERAGE T-VALUE PER CLUSTER
+for j in range(n_clusters):
+    mask = label==j
+    index = np.where(mask)
+    center.append((np.mean(index[0]),np.mean(index[1]),np.mean(index[2])))
+    label_mean[j] =np.mean(data_new[mask])
+
+#PRINT THE PLOTS
+for i in range(data_new.shape[-1]):
+    plt.figure()
+    plt.imshow(data_new[...,i], cmap=plt.cm.gray, interpolation ='nearest')
+    for l in range(n_clusters):
+        plt.contour(label[...,i] == l, contours=1,
+            colors=[plt.cm.spectral(l / float(n_clusters)), ],linewidths= 0.4)
+plt.xticks(())
+plt.yticks(())
+plt.show()
 
 
 
 #####################################
 ####### MULTIPLE TESTING ############
 #####################################
-
-p_mean = np.zeros((64, 64, 34,24))
-
-#loop through each person's T-statistic
-count=0
-for i in sub_list:
-
-    p_stat = np.load(p_data+i+"_pvalue.npy")
-    #mask = nib.load(path_to_data+i+'/anatomy/inplane001_brain_mask.nii.gz')
-    #mask_data = mask.get_data()
-    
-    #p_mean[...,count] = make_mask(p_stat, mask_data, fit=True)
-    
-    p_mean[...,count] = p_stat
-    count+=1
-    
-p_mean = np.mean(p_mean,axis=3)/2
-
-
-p_vals = np.ravel(p_mean).T
-
-print("# ==== No Mask, bh_procedure ==== #")
-# a fairly large false discovery rate
-Q = .4
-significant_pvals = bh_procedure(p_vals, Q)
-
-reshaped_sig_p = np.reshape(significant_pvals, p_mean.shape)
-
-slice_reshaped_sig_p = reshaped_sig_p[...,7]
-
-plt.imshow(slice_reshaped_sig_p)
-plt.colorbar()
-plt.title('Significant p-values (No mask)')
+#
+# p_mean = np.zeros((64, 64, 34,24))
+#
+# #loop through each person's T-statistic
+# count=0
+# for i in sub_list:
+#
+#     p_stat = np.load(p_data+i+"_pvalue.npy")
+#     #mask = nib.load(path_to_data+i+'/anatomy/inplane001_brain_mask.nii.gz')
+#     #mask_data = mask.get_data()
+#
+#     #p_mean[...,count] = make_mask(p_stat, mask_data, fit=True)
+#
+#     p_mean[...,count] = p_stat
+#     count+=1
+#
+# p_mean = np.mean(p_mean,axis=3)/2
+#
+#
+# p_vals = np.ravel(p_mean).T
+#
+# print("# ==== No Mask, bh_procedure ==== #")
+# # a fairly large false discovery rate
+# Q = .4
+# significant_pvals = bh_procedure(p_vals, Q)
+#
+# reshaped_sig_p = np.reshape(significant_pvals, p_mean.shape)
+#
+# slice_reshaped_sig_p = reshaped_sig_p[...,7]
+#
+# plt.imshow(slice_reshaped_sig_p)
+# plt.colorbar()
+# plt.title('Significant p-values (No mask)')
 
 
 
