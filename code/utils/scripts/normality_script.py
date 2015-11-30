cc7a2c710da269ddf4983a3767ff53d4e6744062
Jane Liang <jwliang@berkeley.edu>
2015-11-29 22:08:27 -0800

    Script at least makes sense now.

--- code/utils/scripts/normality_script.py
+++ code/utils/scripts/normality_script.py
@@ -1,11 +1,7 @@
 # Loading modules.
 import numpy as np
-from scipy.stats import gamma
-from scipy.stats import mstats
-import matplotlib.pyplot as plt
 import nibabel as nib
-import numpy.linalg as npl
-import scipy.stats
+from scipy.stats import shapiro
 import os
 import sys
 
@@ -21,10 +17,10 @@ from stimuli import events2neural
 from event_related_fMRI_functions import hrf_single, convolution_specialized
 
 # Load our GLM functions. 
-from glm import glm, glm_diagnostics, glm_multiple
+from glm import glm, glm_diagnostics
 
 # Load our normality functions. 
-from normality import sw, kw
+from normality import check_sw
 
 # Load the image data for subject 1.
 img = nib.load(pathtodata+"BOLD/task001_run001/bold.nii.gz")
@@ -36,23 +32,25 @@ cond2=np.loadtxt(condition_location+"cond002.txt")
 cond3=np.loadtxt(condition_location+"cond003.txt")
 
 
-data.shape #shape of data should be (64, 64, 34, 239)
-
-
 #################
 #np.convolve
 ################
 
-#1D convolved time course
+# initial needed values
 TR = 2
 tr_times = np.arange(0, 30, TR)
 hrf_at_trs = np.array([hrf_single(x) for x in tr_times])
-n_vols = data.shape[-1]
+n_vols=data.shape[-1]
 
-neural_prediction = events2neural(condition_location+"cond001.txt",TR,n_vols)
+# creating the .txt file for the events2neural function
+cond_all=np.row_stack((cond1,cond2,cond3))
+cond_all=sorted(cond_all,key= lambda x:x[0])
+np.savetxt(condition_location+"cond_all.txt",cond_all)
+
+neural_prediction = events2neural(condition_location+"cond_all.txt",TR,n_vols)
 convolved = np.convolve(neural_prediction, hrf_at_trs) # hrf_at_trs sample data
-N = len(neural_prediction)  # N == n_vols == 239
-M = len(hrf_at_trs)  # M == 15
+N = len(neural_prediction)  # N == n_vols == 173
+M = len(hrf_at_trs)  # M == 12
 np_hrf=convolved[:N]
 
 ###################
@@ -67,7 +65,6 @@ np_B, np_X = glm(data, np_hrf)
 ###################################
 
 np_MRSS, np_fitted, np_residuals = glm_diagnostics(np_B, np_X, data)
-np_residuals.shape #should be (64, 64, 34, 239)
 
 ###########################
 #Shapiro-Wilks on Residuals
@@ -75,33 +72,5 @@ np_residuals.shape #should be (64, 64, 34, 239)
 #Shapiro-Wilks: tests the null hypothesis that the data was 
 #drawn from a normal distribution.
 
-normality(resid_4d)
-print "Shapiro-Wilks p-value:", sw_3d #0.0139
-
-##########
-#Plotting
-##########
-
-plt.plot(np_resduals[41, 47, 2]) #from previous scripts
-plt.plot(np_residuals[i,j,k,:])
-plt.plot(resid_4d) #Test statistic from Shapiro-Wilks
-plt.close()
-
-
-#############################
-#Kruskal-Wallis on Residuals
-#############################
-#Kruskal-Wallis: non-parametric method for testing whether 
-#samples originate from the same distribution
-
-normality(resid_4d)
-print "Kruskal-Wallis p-value:", kw_3d
-            
-            
-##########
-#Plotting
-#########
-plt.plot(np_resduals[41, 47, 2]) #from previous scripts
-plt.plot(np_residuals[i,j,k,:]) 
-plt.plot(resid_4d) #Test statistic from Kruskal-Wallis
-plt.close()
+sw_pvals = check_sw(np_residuals)
+print(np.mean(sw_pvals > 0.05))
