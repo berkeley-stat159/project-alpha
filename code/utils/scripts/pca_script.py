aa3a6fea974b021ea4fc37c70ab885e1a6c19eac
Jane Liang <jwliang@berkeley.edu>
2015-12-01 15:10:24 -0800

    Try with masked data.

--- code/utils/scripts/pca_script.py
+++ code/utils/scripts/pca_script.py
@@ -16,14 +16,12 @@ project_path          = "../../../"
 path_to_data          = project_path+"data/ds009/"
 location_of_images    = project_path+"images/"
 location_of_functions = project_path+"code/utils/functions/" 
-behav_suffix           = "/behav/task001_run001/behavdata.txt"
 
 sys.path.append(location_of_functions)
 
-from event_related_fMRI_functions import hrf_single, convolution_specialized
-from noise_correction import mean_underlying_noise, fourier_predict_underlying_noise
+from Image_Visualizing import make_mask
 
-sub_list = os.listdir(path_to_data)[0:2]
+sub_list = os.listdir(path_to_data)
 
 # saving to compare number of cuts in the beginning
 num_cut=np.zeros(len(sub_list))
@@ -39,18 +37,42 @@ for name in sub_list:
     img = nib.load(path_to_data+ name+ "/BOLD/task001_run001/bold.nii.gz")
     data = img.get_data()
 
+    # Load mask.
+    mask = nib.load(path_to_data+ name+'/anatomy/inplane001_brain_mask.nii.gz')
+    mask_data = mask.get_data()
+
     # Drop the appropriate number of volumes from the beginning. 
     first_n_vols=data.shape[-1]
     num_TR_cut=int(first_n_vols-num_TR)
     num_cut[i]=num_TR_cut
     i+=1
     data = data[...,num_TR_cut:] 
-    data_2d = data.reshape((-1,data.shape[-1]))
 
-    # Run PCA on the covariance matrix and plot explained variance.
+    # Now fit a mask to the 3-d image for each time point.
+    my_mask = np.zeros(data.shape)
+    for i in range(my_mask.shape[-1]): 
+         my_mask[...,i] = make_mask(data[...,i], mask_data, fit=True)
+    
+    # Reshape stuff to 2-d (voxel by time) and mask the data. 
+    # This should cut down the number of volumes by more than 50%.
+    my_mask_2d = my_mask.reshape((-1,my_mask.shape[-1]))
+    data_2d = data.reshape((-1,data.shape[-1]))
+    masked_data_2d = data_2d[my_mask_2d.sum(1) != 0,:]
+
+    # PCA analysis on unmasked data: 
+    # Do SVD for the first 20 values of the time by time matrix and plot explained variance.
     pca = PCA(n_components=20)
     pca.fit(data_2d.T.dot(data_2d))
     exp_var = pca.explained_variance_ratio_ 
     plt.plot(range(1,21), exp_var)
     plt.savefig(location_of_images+'pca'+name+'.png')
+    plt.close()
+
+    # PCA analysis on MASKED data: 
+    # Do SVD for the first 20 values of the time by time matrix and plot explained variance.
+    pca_masked = PCA(n_components=20)
+    pca_masked.fit(masked_data_2d.T.dot(masked_data_2d))
+    exp_var_masked = pca_masked.explained_variance_ratio_ 
+    plt.plot(range(1,21), exp_var_masked)
+    plt.savefig(location_of_images+'maskedpca'+name+'.png')
     plt.close()
