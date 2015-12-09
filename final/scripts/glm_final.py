5897df4acb40ad9f02416977fda141f8b80f01a1
hiroto-udagawa <hiroto.udagawa@berkeley.edu>
2015-12-09 10:42:35 -0800

    fixed glm stuff

--- final/scripts/glm_final.py
+++ final/scripts/glm_final.py
@@ -36,9 +36,11 @@ sub_list = os.listdir(path_to_data)[1:]
 
 #Import our functions
 from glm import glm_multiple, glm_diagnostics
-from Image_Visualizing import present_3d
+from Image_Visualizing import present_3d, make_mask
 from noise_correction import mean_underlying_noise, fourier_predict_underlying_noise,fourier_creation
 from hypothesis import t_stat_mult_regression, t_stat
+from mask_phase_2_dimension_change import masking_reshape_start, masking_reshape_end
+
 
 # Progress bar
 toolbar_width=len(sub_list)
@@ -63,6 +65,24 @@ for i in sub_list:
     residual_final = np.zeros((data.shape))
     t_final = np.zeros((data.shape[:-1]))
     p_final = np.zeros((data.shape[:-1]))
+    beta_final = np.zeros((data.shape[:-1]))
+    
+    mask = nib.load(path_to_data+i+'/anatomy/inplane001_brain_mask.nii.gz')
+    mask_data = mask.get_data()
+    mask_data = make_mask(np.ones(data.shape[:-1]), mask_data, fit=True)
+    mask_data = mask_data!=0
+    mask_data = mask_data.astype(int)
+
+    ###PCA SHIT###
+
+    to_2d= masking_reshape_start(data,mask)
+    # double_centered_2d
+    X_pca= to_2d - np.mean(to_2d,0) - np.mean(to_2d,1)[:,None]
+
+    cov = X_pca.T.dot(X_pca)
+
+    U, S, V = npl.svd(cov)
+    pca_addition= U[:,:6] # ~40% coverage
     
     #Run per slice in order to correct for time
     for j in range(data.shape[2]):
@@ -70,27 +90,30 @@ for i in sub_list:
         data_slice = data[:,:,j,:]
         
         #Create design matrix
-        X = np.ones((n_vols,7))
+        X = np.ones((n_vols,9))
         X[:,1] = convolve[:,j]
         X[:,2]=np.linspace(-1,1,num=X.shape[0]) #drift
-        X[:,3:]=fourier_creation(X.shape[0],2)[:,1:]
+        X[:,3:] = pca_addition
+        
         
         beta,t,df,p = t_stat_mult_regression(data_slice, X)
-        
         
         t = t[1,:]
         p = p[1,:]
         
         MRSS, fitted, residuals = glm_diagnostics(beta, X, data_slice)
         
+        beta = beta[:,1]
         
+        beta_final[:,:,j] = beta.reshape(data_slice.shape[:-1])
         t_final[:,:,j] = t.reshape(data_slice.shape[:-1])
         p_final[:,:,j] = p.reshape(data_slice.shape[:-1])        
         residual_final[:,:,j,:] = residuals.reshape(data_slice.shape)
-        
-        np.save("../data/t_stat/"+i+"_tstat.npy", t_final)
-        np.save("../data/residual/"+i+"_residual.npy", residual_final)
-        np.save("../data/p-values/"+i+"_pvalue.npy", p_final)
+    
+    np.save("../data/betas/"+i+"_beta.npy", beta_final)    
+    np.save("../data/t_stat/"+i+"_tstat.npy", t_final)
+    np.save("../data/residual/"+i+"_residual.npy", residual_final)
+    np.save("../data/p-values/"+i+"_pvalue.npy", p_final)
         
      
     sys.stdout.write("-")
