938b97c437cf9f1c1d286823b9ce0f9d9045304d
Rachel Lee <rachel6394@berkeley.edu>
2015-12-09 23:52:43 -0800

    add test for when binary = False

--- code/utils/tests/test_tgrouping.py
+++ code/utils/tests/test_tgrouping.py
@@ -64,6 +64,7 @@ def test_2():
 						prop = True, abs_on = True, binary = False, off_value = 0, masked_value = .5)
 	assert(output5 == False)
 
-	# Test for when not binary
-	#assert()
-
+	# Test for when binary == False
+	output6 = t_grouping_neighbor(x4, mask, .5, neighbors = None,
+						prop = True, abs_on = True, binary = False, off_value = 0, masked_value = .5)
+	assert(np.sum(output6[0]) == 66)
