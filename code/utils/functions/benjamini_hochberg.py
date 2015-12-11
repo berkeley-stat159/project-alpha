f1b6a7cb4ad74c4a07081bb71f91914dfce350ed
Rachel Lee <rachel6394@berkeley.edu>
2015-12-10 16:32:24 -0800

    add a test for BH function

--- code/utils/functions/benjamini_hochberg.py
+++ code/utils/functions/benjamini_hochberg.py
@@ -1,8 +1,6 @@
 from scipy.stats import t as t_dist
 from glm import glm
 import numpy as np
-#import numpy.linalg as npl
-#from hypothesis import t_stat
 
 # Order the p-values from smallest to largest
 # p-value has a rank that is the index of itself in the ordered array
@@ -35,21 +33,16 @@ def bh_procedure(p_vals, Q):
 	# Multiply an array of rank values by k
 	upper = k*np.fromiter(range(1, 1 + p_vals.shape[0]), dtype = "int")
 
-	#print(p_vals.shape)
 	p_sorted = np.sort(p_vals, axis = 0)
-	#print(p_sorted.shape)
-	#p_sorted = np.ravel(p_sorted)
-	#print(p_sorted.shape)
+
 	bool_array = np.zeros(p_sorted.shape[0], dtype = bool)
 	for i in range(p_sorted.shape[0]):
 		if p_sorted[i] < upper[i]:
 			bool_array[i] = True
 
 	# Find maximum True index and the element in it from p_sorted
-
-	# check that bool_array has some True!!!
 	indices = np.where(bool_array)
-	#print(indices)
+	
 	# Make sure there are indices that returned True!!
 	if sum(indices[0]) != 0:
 		max_true_index = np.max(indices)
@@ -57,10 +50,8 @@ def bh_procedure(p_vals, Q):
 		max_upper = np.ravel(p_sorted)[max_true_index]
 	# If no indices where p < upper 
 	else:
-		#max_upper = 0
 		print("**** Oh no. No p-values smaller than upper bound FDR were found. ****")
 		return p_vals
-	#max_upper = p_sorted[np.max(np.where(bool_array))]
 
 
 	# Make all non-siginificant p-values zero
