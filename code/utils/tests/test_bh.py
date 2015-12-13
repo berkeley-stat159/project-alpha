4e76054ec31ffec7ac59c1c23b130b3ebfc29477
hiroto-udagawa <hiroto.udagawa@berkeley.edu>
2015-12-13 14:45:57 -0800

    fixing travis issue

--- code/utils/tests/test_bh.py
+++ code/utils/tests/test_bh.py
@@ -9,11 +9,8 @@ import numpy as np
 import nibabel as nib
 import os
 import sys
-
 from nose.tools import assert_equals
 from numpy.testing import assert_almost_equal, assert_array_equal
-
-
 
 
 # Path to the subject 009 fMRI data used in class. 
