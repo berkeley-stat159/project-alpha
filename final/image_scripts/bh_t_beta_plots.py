e7e471f3a86807b6de369e979ac156fa527b4127
hiroto-udagawa <hiroto.udagawa@berkeley.edu>
2015-12-13 19:14:00 -0800

    various fixes. all of the script stuff works now

--- final/image_scripts/bh_t_beta_plots.py
+++ final/image_scripts/bh_t_beta_plots.py
@@ -32,8 +32,8 @@ final_beta = np.load("../data/bh_t_beta/final_beta_average.npy")
 # Benjamini Hochberg Plots Q = 0.25 #
 #####################################
 
-plt.imshow(present_3d(final_bh), interpolation = 'nearest', cmap = 'seismic')
-plt.title("Mean BH Value Across 25 Subjects with Q = .25")
+plt.imshow(present_3d(final_bh), interpolation = 'nearest', cmap = 'gray')
+plt.title("Mean BH Value Across 24 Subjects with Q = .25")
 plt.colorbar()
 plt.savefig("../../images/bh_mean_final.png")
 plt.close()
@@ -42,21 +42,25 @@ plt.close()
 # T-statistic Plots Proportion = 0.1 #
 ######################################
 
-plt.imshow(present_3d(final_t), interpolation = 'nearest', cmap = 'seismic')
-plt.title("Mean t_grouping Value Across 25 Subjects with proportion = .1")
+
+desired_image=present_3d(final_t)
+desired_image[320:,256:]=.5
+
+plt.imshow(desired_image, interpolation = 'nearest', cmap = 'gray')
+plt.title("Mean t_grouping Value Across 24 Subjects with proportion = .1")
 plt.colorbar()
 plt.savefig("../../images/tgroup_mean_final.png")
 plt.close()
+
 
 ######################################
 # Beta-value Plots Proportion = 0.2  #
 ######################################
 
-plt.imshow(present_3d(final_beta), interpolation = 'nearest', cmap = 'seismic')
-plt.title("Mean beta_grouping Value Across 25 Subjects with proportion = .2")
+plt.imshow(present_3d(final_beta), interpolation = 'nearest', cmap = 'gray')
+plt.title("Mean beta_grouping Value Across 24 Subjects with proportion = .2")
 plt.colorbar()
 plt.savefig("../../images/betagroup_mean_final.png")
 plt.close()
-
 
 
