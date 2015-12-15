b9b2069cba193b4e0b81e03782d54f87b7a4935c
Jane Liang <jwliang@berkeley.edu>
2015-12-15 15:36:13 -0800

    Fix subject paths.

--- final/data_scripts/bh_t_beta_final.py
+++ final/data_scripts/bh_t_beta_final.py
@@ -27,7 +27,8 @@ hrf_data              = final_data + 'hrf/'
 sys.path.append(location_of_functions)
 
 # list of subjects
-sub_list = os.listdir(path_to_data)[1:]
+sub_list = os.listdir(path_to_data)
+sub_list = [i for i in sub_list if 'sub' in i]
 
 # Progress bar
 toolbar_width = len(sub_list)
